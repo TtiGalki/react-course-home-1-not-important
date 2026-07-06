7cd586ac77cb7ee6915d9593ad729100ba60cbe8
TtiGalki <av.trebunskih@gmail.com>
2026-07-06 20:53:20 +0300

    json dinamic cart

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,20 @@
 import Profile from './Profile.jsx'
 import './App.css'
 import Grid from './Grid.jsx'
+import primarchData from '../primarchData.json'
+const images = import.meta.glob('../assets/*.webp', { eager: true, import: 'default' })
+console.log(images)
 
 function App() {
 
   return (
     <>
-    <Profile />
+    <Profile name={primarchData.primarchname}
+             tag={primarchData.tag}
+             location={primarchData.location}
+             image={images[`../assets/${primarchData.avatar}`]}
+             info={primarchData.stats}
+    />
     <Grid />
     </>
   )
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,30 +1,28 @@
-
-import LionElJonson from '../assets/Lion-ElJonson.webp'
-export default function Profile(){
+export default function Profile({ name, tag, location, image, info }) {
     return (
         <div className="profile">
             <div className="profile_top">
                 <img
                     className="profile_avatar"
-                    src = {LionElJonson}
+                    src = {image}
                     alt = "User avatar"
                 />
-                <p className='profile_name'>Lion El'Jonson</p>
-                <p className='profile_top_info'>@Dark</p>
-                <p className='profile_top_info'>Caliban, Imperium</p>
+                <p className='profile_name'>{name}</p>
+                <p className='profile_top_info'>@{tag}</p>
+                <p className='profile_top_info'>{location}</p>
             </div>
             <ul className="profile_info">
                 <li className="profile_item">
                     <span>Legio</span>
-                    <span>100 000</span>
+                    <span>{info.legio}</span>
                 </li>
                 <li className="profile_item">
                     <span>Fratricide</span>
-                    <span>0?</span>
+                    <span>{info.fratricide}</span>
                 </li>
                 <li className="profile_item">
                     <span>Back in 40k</span>
-                    <span>Yes</span>
+                    <span>{info.back40k ? "Yes" : "No"}</span>
                 </li>
             </ul>
         </div>
