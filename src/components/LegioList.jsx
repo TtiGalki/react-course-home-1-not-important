28d3d7a2c9d5f72b062eef1088b88d25791f0b7b
TtiGalki <av.trebunskih@gmail.com>
2026-07-11 14:39:41 +0300

    home 2 full

--- src/components/LegioList.jsx
+++ src/components/LegioList.jsx
@@ -1,15 +1,18 @@
 import FriendListItem from './LegioListItem.jsx'
+import styles from './LegioList.module.css'
 
 export default function LegioList({legions}) {
     return(
-        <ul>
-            <li>
-                {
-                    legions.map((legio) => {
-                        return <FriendListItem avatar={legio.avatar} name={legio.name} isLoyal={legio.isLoyal} />
-                    })
-                }
-            </li>
+        <ul className={styles.listOfLegions}>
+            {
+                legions.map((legio) => {
+                    return (
+                        <li className={styles.legioItem} key={legio.id} >
+                            <FriendListItem avatar={legio.avatar} name={legio.name} isLoyal={legio.isLoyal} />
+                        </li>
+                    )
+                })
+            }
         </ul>
     )
 }
--- src/components/LegioListItem.jsx
+++ src/components/LegioListItem.jsx
@@ -1,11 +1,14 @@
+import styles from './LegioListItem.module.css'
+import clsx from 'clsx';
+
 const images = import.meta.glob('../assets/*.webp', { eager: true, import: 'default' })
 
 export default function LegioListItem({avatar, name, isLoyal}){
     return (
         <div>
-            <img src={images[`../assets/${avatar}`]} alt="Avatar" width="48" />
-            <p>{name}</p>
-            {isLoyal ? (<p>Loyal</p>) : (<p>Traiters</p>)}
+            <img src={images[`../assets/${avatar}`]} alt="Avatar" />
+            <p className={styles.name} >{name}</p>
+            {isLoyal ? (<p className={clsx(styles.loyal, styles.status)} >Loyal</p>) : (<p className={clsx(styles.traiters, styles.status)} >Traiters</p>)}
             
         </div>
 
